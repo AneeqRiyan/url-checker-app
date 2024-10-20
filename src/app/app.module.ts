ed4eb4caf14a1526a5b1b5159ec0e9dbb411c296
Aneeq Ahmed <aneeqriyan@gmail.com>
2024-10-20 15:28:51 +0200

    code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { UrlCheckerComponent } from './components/url-checker.component'; // Import UrlCheckerComponent
-import { AppComponent } from './app.component'; // Import AppComponent
+import { UrlCheckerComponent } from './components/url-checker.component'; 
+import { AppComponent } from './app.component'; 
 
 @NgModule({
-  declarations: [AppComponent, UrlCheckerComponent], // Declare components here
-  imports: [BrowserModule, ReactiveFormsModule], // Import necessary modules
+  declarations: [AppComponent, UrlCheckerComponent], 
+  imports: [BrowserModule, ReactiveFormsModule], 
   bootstrap: [AppComponent]
 })
 export class AppModule {}
